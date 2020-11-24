65568bbb5065b350792c85044fe2bb5da7e7c2f2
Devaraj Premalatha <devaraj4devops@gmail.com>
2020-11-24 21:25:50 +0530

    moved all the files to github

---
+++ deva_sample_ear/src/main/java/deva_sample_ear/App.java
@@ -0,0 +1,13 @@
+package deva_sample_ear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ welcome.java
@@ -0,0 +1,16 @@
+class welcome
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to DevOps!" );
+    }
+}
+
+
+class welcome_devops
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Welcome to DevOps!");
+  }
+}
